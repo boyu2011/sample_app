043d6ce146ee4f80d93860b2f13e268f78cba8cd
Bo Yu <boyu2011@gmail.com>
2011-08-15 16:06:33 -0400

    Working user signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	# For access signed_in? from sessions_helper.rb
+	include SessionsHelper
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,26 @@
 class SessionsController < ApplicationController
-  def new
+  
+	def new
+		@title = "Sign in"
   end
+
+	# POST /sessions
+	# create a new session
+	def create
+		user = User.authenticate(params[:session][:email],
+														 params[:session][:password])
+		if user.nil?
+			# handle errors
+			flash.now[:error] = "Invalid email/password combination."
+			render 'new'
+		else
+			# handle successful signin
+			sign_in user
+			redirect_to user
+		end
+	end
+
+	def destroy
+	end
 
 end
